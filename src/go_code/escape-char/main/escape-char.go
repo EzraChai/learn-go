4da158b9df0987993a81addfdc170ed41d2ac571
EzraChai <juanzhe2@gmail.com>
2022-03-03 23:39:00 +0800

    first day

---
+++ src/go_code/escape-char/main/escape-char.go
@@ -0,0 +1,20 @@
+package main
+
+//"format"
+import "fmt"
+
+func main(){
+	fmt.Println("tom\tjack")
+
+	fmt.Println("Hello\nWorld")
+
+	fmt.Println("\\home\\ezrachai\\Documents\\Go\\src\\go_code\\escape-char\\main")
+
+	//I love you.
+	fmt.Println("\"I ❤️  you.\"Ezra Chai said to Chloe Gan.")
+
+	fmt.Println("sknfsngoenflfngueoiskbdv\n\rdsfks")
+
+	//Homework
+	fmt.Println("\nName\tAge\tStates\tCity\nJohn\t12\tJohor\tKulai")
+}

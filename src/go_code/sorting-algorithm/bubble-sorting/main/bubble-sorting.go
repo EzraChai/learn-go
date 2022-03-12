00575ad8339fcb1dedf0027583754d259781a8f2
EzraChai <juanzhe2@gmail.com>
2022-03-12 18:40:02 +0800

    7th day

--- src/go_code/sorting-algorithm/bubble-sorting/main/bubble-sorting.go
+++ src/go_code/sorting-algorithm/bubble-sorting/main/bubble-sorting.go
@@ -5,27 +5,23 @@ import "fmt"
 func main() {
 	arr := []int{24, 69, 80, 57, 13}
 
-	arr = bubbleSort(arr)
+	bubbleSort(arr)
+
 	fmt.Println(arr)
 }
 
 //	Sort from small number to big number
-func bubbleSort(arr []int) []int {
+func bubbleSort(arr []int) {
 	for i := 0; i < len(arr)-1; i++ {
-		//	First number as larger number
-		largerNum := arr[0]
 		for j := 1; j < len(arr)-i; j++ {
 			//	Check if the front number is bigger than the current Number
 			//	If the front number is bigger than the current number, switch the position
-			if largerNum > arr[j] {
+			if arr[j-1] > arr[j] {
 				//	Switch the position of the number
 				arr[j-1] += arr[j]
 				arr[j] = arr[j-1] - arr[j]
 				arr[j-1] -= arr[j]
 			}
-			//	change the larger number to the current Number
-			largerNum = arr[j]
 		}
 	}
-	return arr
 }

a2d1d21493749d71ca635fc36cd9cb93ef34fd9e
EzraChai <juanzhe2@gmail.com>
2022-03-13 22:34:54 +0800

    8th day

--- src/go_code/struct/demo-detail05/main/struct.go
+++ src/go_code/struct/demo-detail05/main/struct.go
@@ -6,14 +6,17 @@ type Person struct {
 	Name string `json:"name"`
 }
 
-//	like =>> this.xx
-func (p Person) printName() {
+//	 like =>> this.xx
+func (p Person) printName() Person {
 	fmt.Println("printName() =>", p.Name)
+	p.Name = "Hi, " + p.Name
+	return p
 }
 
 func main() {
 	var p1 Person
 	p1.Name = "Chloe Gan"
-	p1.printName()
+	p1 = p1.printName()
+	fmt.Println(p1.Name)
 
 }

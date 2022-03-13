28f8dcf71741b596eb3901972511d80f71af83b7
EzraChai <juanzhe2@gmail.com>
2022-03-14 01:08:16 +0800

    8th day

--- src/go_code/struct/method-details/main/method.go
+++ src/go_code/struct/method-details/main/method.go
@@ -4,6 +4,15 @@ import "fmt"
 
 type integer int
 
+type Student struct {
+	Name string
+	Age  int
+}
+
+func (student *Student) String() string {
+	return fmt.Sprintf("Name=[%v] Age=[%v]", student.Name, student.Age)
+}
+
 func (i *integer) print() {
 	fmt.Println("i =", *i)
 }
@@ -17,4 +26,11 @@ func main() {
 	i.print()
 	i.numberAdd1()
 	i.print()
+
+	stu := Student{
+		Name: "Chloe Gan",
+		Age:  0,
+	}
+	//	If you implement the String() function, fmt.Println() will use the String() function
+	fmt.Println(&stu)
 }

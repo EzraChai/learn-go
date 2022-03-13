46e7183f46c5afdb9cb480410b1b7fd2c03601f9
EzraChai <juanzhe2@gmail.com>
2022-03-14 00:59:13 +0800

    8th day

--- src/go_code/struct/exercise02/main/struct.go
+++ src/go_code/struct/exercise02/main/struct.go
@@ -10,16 +10,30 @@ type Circle struct {
 	Radius int `json:"radius"`
 }
 
-func (circle Circle) area() float64 {
+func (circle *Circle) area() float64 {
+	return math.Pi * float64(circle.Radius*circle.Radius)
+}
+
+//	To increase the efficiency, we will use pointer
+func (circle *Circle) areaV2() float64 {
+	//circle.Radius = 20
+
+	// Pointer = 0xc00001e0a8
+	fmt.Printf("Pointer = %p \n", circle)
+
 	return math.Pi * float64(circle.Radius*circle.Radius)
 }
 
 //	Calculate area of a circle
 func main() {
 	var c1 Circle
-	c1.Radius = 4
+	c1.Radius = 7
 	//	Find the area of the circle with the radius of 4 cm
-	area := c1.area()
+	area := c1.areaV2()
+
+	// 	Pointer = 0xc00001e0a8
+	//	Both pointer in the main function and the areaV2 function are the same
+	fmt.Printf("Pointer = %p \n", &c1)
 
 	//	Format the area to four decimal places
 	areaInString := fmt.Sprintf("%.4f", area)
@@ -27,4 +41,7 @@ func main() {
 	area, _ = strconv.ParseFloat(areaInString, 64)
 	//	Area = 50.2655cm^2, Radius = 4cm
 	fmt.Println(area)
+
+	//	20
+	//fmt.Println(c1)
 }

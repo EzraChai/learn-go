41d273e711f324156557aa88f24deb4d22470b86
EzraChai <juanzhe2@gmail.com>
2022-03-25 21:48:39 +0800

    15th day

--- src/go_code/data_structure/sparse-array/queue-method-1/main/queue.go
+++ src/go_code/data_structure/sparse-array/queue-method-1/main/queue.go
@@ -9,20 +9,20 @@ type queue struct {
 	maxSize    int
 }
 
-func (q *queue) setQueueElement(num int) {
-	q.rear++
-	if q.rear > q.maxSize {
+func (q *queue) SetQueueElement(num int) {
+	if q.rear+1 > q.maxSize {
 		fmt.Println("index out of bounds")
 		return
 	}
+	q.rear++
 	q.queueArray[q.rear] = num
 }
 
-func (q *queue) getQueueElement() int {
+func (q *queue) GetQueueElement() int {
 	q.front++
 	if q.front > q.rear {
 		fmt.Println("no more elements")
-		return -1
+		return 0
 	}
 	num := q.queueArray[q.front]
 	q.queueArray[q.front] = 0
@@ -41,15 +41,17 @@ func InitQueue() *queue {
 //	Using array to demonstrate the queue
 func main() {
 	queue := InitQueue()
-	for i := 0; i < 5; i++ {
-		queue.setQueueElement(i)
+	for i := 1; i <= 5; i++ {
+		queue.SetQueueElement(i)
 	}
 	fmt.Println(queue.queueArray)
 	for i := 0; i < 3; i++ {
-		fmt.Println(queue.getQueueElement())
+		fmt.Println(queue.GetQueueElement())
 	}
 	fmt.Println(queue.queueArray)
-	fmt.Println(queue.getQueueElement())
+	fmt.Println(queue.GetQueueElement())
 	fmt.Println(queue.queueArray)
+	fmt.Println(queue.GetQueueElement())
+	//queue.SetQueueElement(6)
 
 }
